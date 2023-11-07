721d8ed90a475a53db73676f6b133f55d942f0fb
anasa <anas-aji-seeroo>
2023-11-07 19:25:59 +0000

    backed added

---
+++ yTubeBackend/src/test/java/com/anasajimuhammed/ytubebackend/YTubeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anasajimuhammed.ytubebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YTubeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
