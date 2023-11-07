b3160cb2231554100beafe618f63e32effe96213
anasa <anas-aji-seeroo>
2023-11-07 19:12:40 +0000

    Backend Added

---
+++ Backend/src/main/java/com/anasajimuhammed/ytube/YtubeApplication.java
@@ -0,0 +1,13 @@
+package com.anasajimuhammed.ytube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtubeApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/anasajimuhammed/ytube/YtubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anasajimuhammed.ytube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
